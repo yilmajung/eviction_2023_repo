59996f11438afe9d97517eefb2aaa6c760b591c6
yilmajung <woo.firstrunner@gmail.com>
2024-01-11 16:53:57 -0500

    Took out the imputation part from stan file

--- SGLMM_eviction4_other_mice.R
+++ SGLMM_eviction4_other_mice.R
@@ -4,28 +4,27 @@ library(sf)
 #install.packages('spdep')
 library(spdep)
 library(readr)
-library(mice)
-#rm(list=ls())
+rm(list=ls())
 # Load the main data
-df <- read_csv("data/acs_evic_data_wo_zero_pop.csv")
-#df_np <- read_csv("data/acs_evic_data_np_wo_zero_pop.csv")
-head(df)
-df_geom <- df
-#df_np_geom <- df_np
+#df <- read_csv("data/acs_evic_data_wo_zero_pop.csv")
+df_np <- read_csv("data/acs_evic_data_np_wo_zero_pop.csv")
+
+df_np_geom <- df_np
 
 # Convert df_geom to SF object using geometry information
-df_geom <- st_as_sf(df_geom, wkt = "geometry_x")
-#df_np_geom <- st_as_sf(df_np_geom, wkt = "geometry_x")
+df_np_geom <- st_as_sf(df_np_geom, wkt = "geometry_x")
 
 # Read the CBG shapefile
 # cbg <- st_read("/Users/wooyongjung/WJ_Projects/eviction_geospatial/data/tl_2022_48_bg/tl_2022_48_bg.shp")
 
 # Create a neighbors list from the census tract BG polygons
-neighbors_list <- poly2nb(df_geom$geometry_x)
+neighbors_list <- poly2nb(df_np_geom$geometry_x)
 
 # Create the binary adjacency matrix, with "B" for binary
 A <- nb2mat(neighbors_list, style='B', zero.policy=TRUE)
 dim(A)
+mean(colSums(A))
+
 
 # Create a sparse matrix for A
 neighbors <- which(A==1, arr.ind=TRUE)
@@ -44,7 +43,7 @@ covariates <- c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_c
 
 
 # Impute missing values
-df2 <- df[covariates]
+df2 <- df_np[covariates]
 colnames(df2) <- c("poverty_rate", "gross_rent_mt40", "hh_social_programs", "hh_w_child_ratio", "unemployment_rate",
                    "black_ratio", "white_ratio", "asian_ratio", "hispanic_ratio", "edu_lt_highschool", "median_age", 
                    "hh_nonfamily_ratio", "renter_occ_rate", "mortgage_status_ratio", "oneunit_structure_ratio", 
@@ -53,14 +52,16 @@ colnames(df2) <- c("poverty_rate", "gross_rent_mt40", "hh_social_programs", "hh_
                    "hh_average_size_renter_occupied")
 head(df2)
 summary(df2)
+#install.packages('mice')
+library(mice)
 
 # Check missing values pattern
 md.pattern(df2)
 
 # Impute missing values using MICE
-imputed <- mice(df2, m=5, maxit=50, method='pmm', seed=123)
-df3 <- complete(imputed, 2)
-summary(df3)
+df2 <- mice(df2, m=5, maxit=50, method='pmm', seed=500)
+df2 <- complete(df2, 2)
+summary(df2)
 
 # Standardize predictors
 covariates <- c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_child_ratio', 
@@ -69,17 +70,17 @@ covariates <- c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_c
                 'renter_occ_rate', 'mortgage_status_ratio', 'oneunit_structure_ratio', 'vacancy_rate', 
                 'median_gross_rent_change', 'time_to_work_lt30', 'time_to_work_30to59', 'time_to_work_mt60',
                 'hh_median_income', 'median_gross_rent',  'housing_median_value', 'hh_average_size_renter_occupied')
-df3[covariates] <- scale(df3[covariates])
-summary(df3)
-df3 <- cbind(df$case_number, df$eviction_rate, df3)
-colnames(df3)[1:2] <- c("case_number", "eviction_rate")
+df2[covariates] <- scale(df2[covariates])
+summary(df2)
+df2 <- cbind(df_np$case_number, df_np$eviction_rate, df2)
+colnames(df2)[1:2] <- c("case_number", "eviction_rate")
 
 #tempdir()
 #dir.create(tempdir())
 
 # Calculate P_perp
 # Create a design matrix
-X <- as.matrix(df3[,c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_child_ratio', 
+X <- as.matrix(df2[,c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_child_ratio', 
                       'unemployment_rate', 'black_ratio', 'white_ratio', 'asian_ratio', 
                       'hispanic_ratio', 'edu_lt_highschool','median_age', 'hh_nonfamily_ratio', 
                       'renter_occ_rate', 'mortgage_status_ratio', 'oneunit_structure_ratio', 'vacancy_rate', 
@@ -95,15 +96,13 @@ XX_inv <- solve(t(X) %*% X, tol = 3e-18)
 
 # Compute P_perp
 P_perp <- I - X %*% XX_inv %*% t(X)
-XX_inv_tx <- XX_inv %*% t(X)
 dim(P_perp)
-dim(XX_inv_tx)
-P_perp[1:10, 1:10]
+
 # Set up the data list for Stan (Y = eviction_rate)
-stan_data <- list(N = nrow(df3), 
+stan_data <- list(N = nrow(df2), 
                   K = 24,
-                  Y = df3$eviction_rate,
-                  X = as.matrix(df3[,c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_child_ratio', 
+                  Y = df2$eviction_rate,
+                  X = as.matrix(df2[,c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_child_ratio', 
                                        'unemployment_rate', 'black_ratio', 'white_ratio', 'asian_ratio', 
                                        'hispanic_ratio', 'edu_lt_highschool','median_age', 'hh_nonfamily_ratio', 
                                        'renter_occ_rate', 'mortgage_status_ratio', 'oneunit_structure_ratio', 'vacancy_rate', 
@@ -111,20 +110,19 @@ stan_data <- list(N = nrow(df3),
                                        'hh_median_income', 'median_gross_rent',  'housing_median_value', 'hh_average_size_renter_occupied')]),
                   A = A,
                   P_perp = P_perp,
-                  XX_inv_tx = XX_inv_tx,
                   num_neighbors=colSums(A),
-                  tau = 10)
+                  tau = 1)
+
 
 # Fit the model
 fit <- stan(file = 'sglmm_orthog_mice.stan', data = stan_data, 
-            iter=4000, chains=4, cores=2, warmup=2000)
+            iter=2000, chains=4, cores=2, warmup=1000)
 
 summary(fit)
 getwd()
 # Extract the results
 posterior_estimates <- rstan::extract(fit)
 
-stan_trace(fit)
 # Extract the fixed effects coefficients
 c('poverty_rate', 'gross_rent_mt40', 'hh_social_programs', 'hh_w_child_ratio', 
   'unemployment_rate', 'black_ratio', 'white_ratio', 'asian_ratio', 
@@ -158,12 +156,9 @@ medinc_samples <- posterior_estimates$beta[,21]
 medrent_samples <- posterior_estimates$beta[,22]
 medvalue_samples <- posterior_estimates$beta[,23]
 renter_hhsize_samples <- posterior_estimates$beta[,24]
-spatial_effects_samples <- posterior_estimates$W_transformed
 
-dim(posterior_estimates$beta_orig)
 dim(posterior_estimates$beta)
 dim(intercept_samples)
-
 
 # Create a data frame for 4000 samples
 df_samples <- data_frame(intercept_samples, poverty_samples, rent_mt40_samples, social_program_samples,
@@ -172,7 +167,7 @@ df_samples <- data_frame(intercept_samples, poverty_samples, rent_mt40_samples, 
                          medage_samples, nonfam_samples, renter_occ_rate_samples, mort_ratio_samples, 
                          unit1_structure_samples, vacancy_rate_samples, medrent_change_samples,
                          time_to_work_lt30_samples, time_to_work_30to59_samples, time_to_work_mt60_samples,
-                         medinc_samples, medrent_samples, medvalue_samples, renter_hhsize_samples, spatial_effects_samples)
+                         medinc_samples, medrent_samples, medvalue_samples, renter_hhsize_samples, )
 
 df_95ci <- t(sapply(df_samples, function(x) quantile(x, probs = c(0.025, 0.975))))
 df_mean <- data_frame(sapply(df_samples, function(x) mean(x)))
@@ -180,86 +175,35 @@ dim(df_95ci)
 dim(df_mean)
 df_95ci <- cbind(df_95ci, df_mean)
 View(df_95ci)
-
-#########################
-# Add original beta values
-
-intercept_samples <- posterior_estimates$alpha
-poverty_samples <- posterior_estimates$beta_orig[,1]
-rent_mt40_samples <- posterior_estimates$beta_orig[,2]
-social_program_samples <- posterior_estimates$beta_orig[,3]
-hh_w_child_samples <- posterior_estimates$beta_orig[,4]
-unemp_samples <- posterior_estimates$beta_orig[,5]
-black_ratio_samples <- posterior_estimates$beta_orig[,6]
-white_ratio_samples <- posterior_estimates$beta_orig[,7]
-asian_ratio_samples <- posterior_estimates$beta_orig[,8]
-hispanic_ratio_samples <- posterior_estimates$beta_orig[,9]
-edu_lt_hs_samples <- posterior_estimates$beta_orig[,10]
-medage_samples <- posterior_estimates$beta_orig[,11]
-nonfam_samples <- posterior_estimates$beta_orig[,12]
-renter_occ_rate_samples <- posterior_estimates$beta_orig[,13]
-mort_ratio_samples <- posterior_estimates$beta_orig[,14]
-unit1_structure_samples <- posterior_estimates$beta_orig[,15]
-vacancy_rate_samples <- posterior_estimates$beta_orig[,16]
-medrent_change_samples <- posterior_estimates$beta_orig[,17]
-time_to_work_lt30_samples <- posterior_estimates$beta_orig[,18]
-time_to_work_30to59_samples <- posterior_estimates$beta_orig[,19]
-time_to_work_mt60_samples <- posterior_estimates$beta_orig[,20]
-medinc_samples <- posterior_estimates$beta_orig[,21]
-medrent_samples <- posterior_estimates$beta_orig[,22]
-medvalue_samples <- posterior_estimates$beta_orig[,23]
-renter_hhsize_samples <- posterior_estimates$beta_orig[,24]
-spatial_effects_samples <- posterior_estimates$W_transformed
-
-df_samples_orig <- data_frame(intercept_samples, poverty_samples, rent_mt40_samples, social_program_samples,
-                              hh_w_child_samples, unemp_samples, black_ratio_samples, white_ratio_samples,
-                              asian_ratio_samples, hispanic_ratio_samples, edu_lt_hs_samples, 
-                              medage_samples, nonfam_samples, renter_occ_rate_samples, mort_ratio_samples, 
-                              unit1_structure_samples, vacancy_rate_samples, medrent_change_samples,
-                              time_to_work_lt30_samples, time_to_work_30to59_samples, time_to_work_mt60_samples,
-                              medinc_samples, medrent_samples, medvalue_samples, renter_hhsize_samples, spatial_effects_samples)
-
-df_95ci_orig <- t(sapply(df_samples_orig, function(x) quantile(x, probs = c(0.025, 0.975))))
-df_mean_orig <- data_frame(sapply(df_samples_orig, function(x) mean(x)))
-dim(df_95ci_orig)
-dim(df_mean_orig)
-df_95ci_orig <- cbind(df_95ci_orig, df_mean_orig)
-df_95ci <- cbind(df_95ci, df_95ci_orig)
-View(df_95ci)
-################################
-
-
-
-
-write.csv(df_95ci, 'df_95ci_nonpayment_mice.csv')
+write.csv(df_95ci, 'df_95ci_other_factors_mice.csv')
 
 # Extract the spatial random effects
 spatial_effects <- posterior_estimates$W_transformed
 dim(spatial_effects)
 avg_spatial_effects <- apply(spatial_effects, 2, mean)
-df$spatial_effect <- avg_spatial_effects
-df_geom$spatial_effect <- avg_spatial_effects
-write.csv(df_geom, 'df_geom_nonpayment_mice.csv')
+df_np$spatial_effect <- avg_spatial_effects
+df_np_geom$spatial_effect <- avg_spatial_effects
+write.csv(df_np_geom, 'df_geom_other_factors_mice.csv')
 
-class(df_geom)
+class(df_np_geom)
 
-plot1 <- ggplot(df_geom) + geom_sf(aes(fill=spatial_effect, geometry=geometry_x), color=NA) + 
+plot1 <- ggplot(df_np_geom) + geom_sf(aes(fill=spatial_effect, geometry=geometry_x), color=NA) + 
   scale_fill_viridis_c() + labs(title="Spatial Random Effects", fill="Effect") + theme_minimal()
 
 
-ggplot(df_geom) +
+ggplot(df_np_geom) +
   geom_sf(aes(fill=spatial_effect, geometry=geometry_x), color=NA) +
   scale_fill_viridis_c() +
   labs(title="Spatial Random Effects", fill="Effect") +
   theme_minimal()
 
-ggplot(df_geom) +
+ggplot(df_np_geom) +
   geom_sf(aes(fill=spatial_effect), color=NA) +
   scale_fill_gradient2(midpoint=0, low='blue', mid='yellow', high='red', space='Lab') +
   labs(title="Spatial Random Effects", fill="Effect") +
   theme_minimal()
 
-ggplot(df_geom) +
+ggplot(df_np_geom) +
   geom_sf(aes(fill=case_number), color=NA) +
   scale_fill_viridis_c(option="plasma") +
   labs(title="Number of Eviction Filings", fill="# of Eviction") +
@@ -307,10 +251,10 @@ hist(renter_occ_rate_samples, main="Total Households", xlab="Beta_total_househol
 
 # Extract the samples for the beta coefficients
 beta_samples <- extract(fit)$beta
-dim(beta_samples)
+
 # Convert to a data frame for easier plotting with ggplot2
 beta_df <- as.data.frame(t(beta_samples))
-dim(beta_df)
+
 # Rename the columns to match the predictor names
 names(beta_df) <- c("Predictor1", "Predictor2", "Predictor3", "Predictor4")
 
@@ -319,6 +263,7 @@ library(tidyr)
 beta_long <- gather(beta_df, key = "Predictor", value = "Value", factor_key=TRUE)
 
 # Plot using ggplot2
+library(ggplot2)
 ggplot(beta_long, aes(x = 1:NROW(Value), y = Value, color = Predictor)) +
   geom_line() +
   facet_wrap(~Predictor, scales = 'free_y') +
--- sglmm_orthog_mice.stan
+++ sglmm_orthog_mice.stan
@@ -3,8 +3,9 @@ data {
     int<lower=0> K; // Number of predictors
     int<lower=0> Y[N];  // outcome variable for each area
     matrix[N, K] X;  // design matrix for fixed effects (standardized predictors)
-    int<lower=1> A_sparse[4975, 2]; // sparse representation of adjacency matrix
+    matrix<lower=0, upper=1>[N, N] A;  // adjacency matrix, 0 or 1
     matrix[N, N] P_perp; // projection matrix for orthogonalizing spatial random effects
+    matrix[K, N] XX_inv_tx; // projection matrix for calculating original betas
     int<lower=0> num_neighbors[N];  // number of neighbors for each area
     real<lower=0> tau;  // precision parameter for spatial effects
 }
@@ -21,23 +22,19 @@ transformed parameters {
 
 model {
     // Priors
-    alpha ~ normal(0, 1);
-    beta ~ normal(0, 1);
+    alpha ~ normal(0, 30);
+    beta ~ normal(0, 5);
     W ~ normal(0, tau); // Precision parameter for spatial random effects
     
-    // ICAR prior for spatial random effects using sparse adjacency matrix
-    for (n in 1:4975) {
-        int i = A_sparse[n, 1];
-        int j = A_sparse[n, 2];
-        if (num_neighbors[i] > 0) {
-            target += W[i] * W[j];
-        }
-    }
-
-    // Additional ICAR prior component for each area
+    // ICAR prior for spatial random effects
     for (i in 1:N) {
         if (num_neighbors[i] > 0) {
-            target += -0.5 * num_neighbors[i] * square(W[i]);
+        target += -0.5 * num_neighbors[i] * square(W[i]);
+        for (j in 1:N) {
+            if (A[i, j] == 1) {
+            target += W[i] * W[j];
+            }
+        }
         }
     }
   
@@ -45,4 +42,8 @@ model {
     for (i in 1:N) {
         Y[i] ~ poisson_log(alpha + dot_product(X[i], beta) + W_transformed[i]);
     }
+}
+
+generated quantities {
+    vector[K] beta_orig = beta - (XX_inv_tx * W);  // coefficients for predictors
 }
